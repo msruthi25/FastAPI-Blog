5dd626df62e07e0f6c0f63467ba3411ac02fdad0
Sruthi <msruthi251999@gmail.com>
2026-02-11 00:34:42 -0500

    AI BackEnd Changes

--- BackEnd/app/agent/llm.py
+++ BackEnd/app/agent/llm.py
@@ -1,7 +1,7 @@
 from openrouter import OpenRouter
 from .state import State
 
-client = OpenRouter(api_key="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")
+client = OpenRouter(api_key="Add your API Key")
 
 def call_llm(state:State) ->str:
     response = client.chat.send(
